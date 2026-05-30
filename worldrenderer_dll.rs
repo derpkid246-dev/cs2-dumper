7b43df44d4dd5e76d119f55f11e6e8ed44c32fa4
derpkid246-dev <derpkid246@gmail.com>
2026-05-29 19:22:14 -0700

    Add files via upload

--- worldrenderer_dll.rs
+++ worldrenderer_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-22 15:52:41.687650400 UTC
+// 2026-05-30 02:20:36.593484 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
