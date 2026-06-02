4e4681b665274a8d3fcdb98a077327e113e9964c
derpkid246-dev <derpkid246@gmail.com>
2026-06-02 14:22:04 -0700

    Add files via upload

--- host_dll.rs
+++ host_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-30 02:20:36.593484 UTC
+// 2026-06-02 21:20:52.649526 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- soundsystem_dll.hpp
+++ soundsystem_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-30 02:20:36.593484 UTC
+// 2026-06-02 21:20:52.649526 UTC
 
 #pragma once
 
